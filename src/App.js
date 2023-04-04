bfa85169379adf553dd680be6b59ee71c6ec8445
khushbooraikwar95 <j.khushbooraikwar95@gmail.com>
2023-04-04 17:27:07 +0530

    Authentication added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./Post";
+import { db, auth } from "./firebase";
+import Modal from "@mui/material/Modal";
+import { Button, Input } from "@mui/material";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      username: "khush_kr",
-      caption: "wow! It works",
-      imageUrl:
-        "https://www.freecodecamp.org/news/content/images/size/w1000/2023/03/emile-perron-xrVDYZRGdw4-unsplash.jpg",
-    },
-    {
-      username: "iamjiit",
-      caption: "Mmmm looks nice!",
-      imageUrl:
-        "https://www.freecodecamp.org/news/content/images/size/w1000/2023/03/pexels-chelsey-horne-4506938.jpg",
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        //user has logged in...
+        console.log(authUser);
+        setUser(authUser);
+      } else {
+        //user has logged off...
+        setUser(null);
+      }
+    });
+
+    return () => {
+      //perform some cleanup action
+      unsubscribe();
+    };
+  }, [user, username]);
+  // useEffects runs a piece of code based on a specific condiion
+  useEffect(() => {
+    //this is where code runs
+    db.collection("posts").onSnapshot((snapshot) => {
+      //everytime a new post is added in db , this code fires..
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          post: doc.data(),
+        }))
+      );
+    });
+  }, []);
+
+  // const handleOpen = () => setOpen(true);
+  const signUp = (event) => {
+    event.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .the((authUser) => {
+        return authUser.user.updateProfile({
+          displayName: username,
+        });
+      })
+      .catch((error) => alert(error.message));
+  };
 
   return (
     <div className="App">
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={style} className="modal">
+          <form className="app__signup">
+            <center>
+              <img
+                className="app__headerImage"
+                src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                alt=""
+              />
+            </center>
+            <Input
+              placeholder="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Input
+              placeholder="email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              placeholder="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button onClick={signUp}>Sign Up</Button>
+          </form>
+        </div>
+      </Modal>
+
       <div className="app__header">
         <img
           className="app__headerImage"
@@ -27,10 +112,15 @@ function App() {
           alt=""
         />
       </div>
+
+      <Button type="submit" onClick={() => setOpen(true)}>
+        Sign Up
+      </Button>
       <h1>Khush</h1>
 
-      {posts.map((post) => (
+      {posts.map(({ id, post }) => (
         <Post
+          key={id}
           username={post.username}
           caption={post.caption}
           imageUrl={post.imageUrl}
--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-import firebase from "firebase";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const firebaseApp = firebase.initializeApp({
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   apiKey: "AIzaSyCP3HvYF1EBrioCbrWY6mf0SjdJU17NjWM",
   authDomain: "instagram-clone-react-99318.firebaseapp.com",
   databaseURL:
@@ -15,4 +18,6 @@ const firebaseApp = firebase.initializeApp({
 
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
-const storage = firebase.storage();
+// const storage = firebase.storage();
+
+export { db, auth };
